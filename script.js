c5ff14265864251173dede01b53e47b475cbcbdd
mnaseeajit <147361506+mnaseeajit@users.noreply.github.com>
2023-12-21 17:27:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+function activateSearch() {
+            var searchDiv = document.querySelector('.search');
+            searchDiv.classList.add('active');
+
+            var inputElement = document.querySelector('.input');
+            inputElement.focus();
+        }
